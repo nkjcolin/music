5bbaa55f50b692d004b281ac4e60a0a762bd344b
nkjcolin <49198636+nkjcolin@users.noreply.github.com>
2019-07-02 21:19:47 +0800

    Add files via upload

---
+++ ydl.py
@@ -0,0 +1,15 @@
+import youtube_dl
+
+a=input("Enter URL:")
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors':[{
+    'key':'FFmpegExtractAudio',
+    'preferredcodec': 'mp3',
+    'preferredquality': '320',}],
+    
+    }
+    
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([a])
